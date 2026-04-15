291403dbbdcd2ff459b0dd02febaa55b8bf34294
Mihir Ravat <mihirravat9901@gmail.com>
2026-04-15 12:16:09 +0530

    updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '../store/authStore';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: 'https://questify-6xs1.onrender.com',
 });
 
 // Interceptor to inject token into requests
